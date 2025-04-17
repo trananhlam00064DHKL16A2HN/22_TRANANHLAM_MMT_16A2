ca2c025c635b8089a398f8827f8796c9bee13d4a
trananhlam0064 <119118231+trananhlam00064DHKL16A2HN@users.noreply.github.com>
2025-04-17 10:19:21 +0700

    Add files via upload

---
+++ blockchain/Message.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MessageContract {
+    string public message;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
+    function updateMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+}
